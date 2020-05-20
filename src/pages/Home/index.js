eb38efe12a2c14354a85bf3e0cd7c2778b138b2a
Kelvin Musselli <kelvinmusselli22@gmail.com>
2020-05-20 02:20:53 -0300

    WIP

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,58 @@
-import React, { useEffect } from 'react';
-import { Container } from './styles';
+import React, { useState, useEffect } from 'react';
+import {
+  Container,
+  ListPokemons,
+  Pokemons,
+  Title,
+  DivTitle,
+  FormButton,
+} from './styles';
+import api from '../../services/api';
 
 const Home = () => {
-  useEffect(() => {});
+  const [dataPokemons, setListPokemons] = useState([]);
+  const [pages, setPages] = useState({ next: null, prev: null });
+
+  useEffect(() => {
+    const getPokemons = async () => {
+      const data = await api.get('/pokemon');
+      if (data) {
+        setListPokemons(data.data);
+        setPages({ next: data.data.next, prev: data.data.prev });
+      }
+    };
+    getPokemons();
+  }, []);
+  const nextPage = async () => {
+    const data = await api.get(pages.next);
+    if (data) {
+      setListPokemons(data.data);
+      setPages({ next: data.data.next, prev: data.data.prev });
+    }
+  };
+  const prevPage = async () => {
+    const data = await api.get(pages.prev);
+    if (data) {
+      setListPokemons(data.data);
+      setPages({ next: data.data.next, prev: data.data.prev });
+    }
+  };
   return (
     <Container>
-      <h1>Pokedex - React</h1>
+      <DivTitle>
+        <Title>Pokedex - React</Title>
+      </DivTitle>
+      <ListPokemons>
+        {dataPokemons.results
+          ? dataPokemons.results.map((poke, index) => (
+              <Pokemons key={index}>{poke.name}</Pokemons>
+            ))
+          : null}
+      </ListPokemons>
+      <FormButton>
+        <button onClick={() => prevPage()}>Anterior</button>
+        <button onClick={() => nextPage()}>Próxima</button>
+      </FormButton>
     </Container>
   );
 };
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -7,4 +7,47 @@ export const Container = styled.div`
   padding: 0 20px 50px;
   align-content: center;
   justify-content: center;
+  height: 400px;
+  flex-direction: column;
+`;
+
+export const DivTitle = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+export const Title = styled.h1``;
+
+export const ListPokemons = styled.div`
+  display: flex;
+  background: red;
+  height: 100%;
+  width: 100%;
+  margin: 0 auto;
+  padding: 4px;
+  flex-direction: row;
+  flex-wrap: wrap;
+`;
+
+export const Pokemons = styled.div`
+  display: flex;
+  justify-content: center;
+  height: 90px;
+  width: 90px;
+  align-items: center;
+  border: 1px solid #fff;
+  color: #fff;
+`;
+export const FormButton = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: -35px;
+  button {
+    background: none;
+    height: 30px;
+    color: #fff;
+    border: 1px solid #fff;
+    border-radius: 4px;
+    margin: 0 15px;
+  }
 `;
